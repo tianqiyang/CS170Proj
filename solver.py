54827bc705015fe6a6ea0338747c996f685c51ae
Tianqi Yang <tianqiy@berkeley.edu>
2020-04-15 14:03:45 -0700

    fix input files

--- solver.py
+++ solver.py
@@ -1,8 +1,7 @@
 import networkx as nx
-from parse import read_input_file, write_output_file
-from utils import is_valid_network, average_pairwise_distance
+from parse import *
+from utils import *
 import sys
-
 
 def solve(G):
     """
@@ -14,18 +13,26 @@ def solve(G):
     """
 
     # TODO: your code here!
-    pass
+    return G
 
 
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G = read_input_file(path)
-#     T = solve(G)
-#     assert is_valid_network(G, T)
-#     print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-#     write_output_file(T, 'out/test.out')
+def checkValidInput():
+    paths = ["25", "50", "100"]
+    for path in paths:
+        if (not validate_file(path + ".in")):
+            return False
+    return True
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    assert checkValidInput()
+    G = read_input_file(path, 100)
+    T = solve(G)
+    #assert is_valid_network(G, T)
+    print("Average pairwise distance: {}".format(average_pairwise_distance(T)))
+    write_output_file(T, f'{path}.out')
